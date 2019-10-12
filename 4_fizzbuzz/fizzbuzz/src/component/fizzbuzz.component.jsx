810c11c7001234124f72abaeb3033e14ab46204b
Bull-Butcher <markjamesbutcher@gmail.com>
2019-10-12 15:22:58 +1100

    fizzbuzz logic

--- 4_fizzbuzz/fizzbuzz/src/component/fizzbuzz.component.jsx
+++ 4_fizzbuzz/fizzbuzz/src/component/fizzbuzz.component.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 
 
-function FizzBuzz() {
+class FizzBuzz extends React.Component {
 
-    return (
+
+fizzLogic() {
+  return (
+    Array.from({ length: 100 })
+    .map((_, n) => 
+     n === 0 ? 0 : n % 15 === 0? "FizzBuzz" : n % 3 === 0 ? "Fizz" : n % 5 === 0 ? "Buzz" : n
+    )
+    .join(", ")
+  )
+}
+  
+
+render() {
+  return (
       <div>
-      {Array.from({ length: 100 })
-      .map((_, n) => 
-       n === 0 ? 0 : n % 15 === 0? "FizzBuzz" : n % 3 === 0 ? "Fizz" : n % 5 === 0 ? "Buzz" : n
-      )
-      .join(", ")
-      }
+        {this.fizzLogic()}
       </div>
-
-
-    );
+      )};
 }
 
 export default FizzBuzz;
