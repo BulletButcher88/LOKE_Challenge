2f0082d6d4d9de370f0f3e63cb7f70699fa90b5c
Bull-Butcher <markjamesbutcher@gmail.com>
2019-10-13 14:03:20 +1100

    refine function for fizzbuzz logic

--- 4_fizzbuzz/fizzbuzz/src/component/fizzbuzz.component.jsx
+++ 4_fizzbuzz/fizzbuzz/src/component/fizzbuzz.component.jsx
@@ -3,17 +3,22 @@ import React from 'react';
 
 class FizzBuzz extends React.Component {
 
-
+  // 
 fizzLogic() {
+
+  const NumArray = Array.from({ length: 100 });
+  
   return (
-    Array.from({ length: 100 })
-    .map((_, n) => 
-     n === 0 ? 0 : n % 15 === 0? "FizzBuzz" : n % 3 === 0 ? "Fizz" : n % 5 === 0 ? "Buzz" : n
-    )
-    .join(", ")
+    NumArray.map((_, n) => 
+      n === 0 ? 0 :
+      n % 15 === 0? "FizzBuzz" :
+      n % 3 === 0 ? "Fizz" :
+      n % 5 === 0 ? "Buzz" :
+      n
+    ).join(", ")
   )
 }
-  
+
 
 render() {
   return (
